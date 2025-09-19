005bceb82e231f3596c5b38db64bb58ee8317888
Tito Parizotto <32302233+titoparizotto@users.noreply.github.com>
2025-09-19 18:48:24 -0300

    Build and Deploy (#10)
    * dev: Condicional para que realize build quando for push ou pull_request.merged
    
    * dev: alteração da taxonomia do codebuilder
    
    * dev: Mofificação para dependência do lncr-core para utils de seegurança (token)
    adicionado GITHUB_ACTOR e GITHUB_TOKEN para pipeline
    
    * dev: Mofificação para dependência do lncr-core para utils de seegurança (token)
    adicionado GITHUB_ACTOR e GITHUB_TOKEN para pipeline
    
    * dev: Mofificação para dependência do lncr-core para utils de seegurança (token)
    adicionado GITHUB_ACTOR e GITHUB_TOKEN para pipeline
    
    * dev: Mofificação para dependência do lncr-core para utils de seegurança (token)
    adicionado GITHUB_ACTOR e GITHUB_TOKEN para pipeline
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Atualização de settings.xml e pom.xml
    
    * dev: Remoção do distributionManagment, validação via credenciais do github (GITHUB_ACTOR, GITHUB_TOKEN)
    
    * dev: Remoção do distributionManagment, validação via credenciais do github (GITHUB_ACTOR, GITHUB_TOKEN)
    
    * dev: Adicionado fluxo de deploy manual
    
    * dev: Adicionado fluxo de deploy manual
    
    * dev: Adicionado fluxo de deploy manual
    
    * dev: Adicionado fluxo de deploy manual
    
    * dev: Adicionado fluxo de deploy manual
    
    * dev: Adaptado o DockerFile para realizar build com maven_settings.xml correto
    
    * dev: Implementada SecretUtils.java e TokenDecoderUtils.java que possui acoplamento com AWS
    
    * dev: Mapeamento de variáveis de ambientes no pipeline
    Mudando settings.xml para maven_settings.xml
    
    * dev: Mapeamento de variáveis de ambientes no pipeline
    Mudando settings.xml para maven_settings.xml
    
    * dev: Mapeamento de variáveis de ambientes no pipeline
    Mudando settings.xml para maven_settings.xml
    
    * dev: Tradução das pipelines e renomeando arquivos

--- aws-lambda/CustomAuthorizer/src/main/java/br/com/tp/lncr/aws/lambda/model/RequestDTO.java
+++ aws-lambda/CustomAuthorizer/src/main/java/br/com/tp/lncr/aws/lambda/model/RequestDTO.java
@@ -2,33 +2,18 @@ package br.com.tp.lncr.aws.lambda.model;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2CustomAuthorizerEvent;
 
-import java.util.Map;
-
 public class RequestDTO {
     private final String resource;
     private final String httpMethod;
     private final TokenClaims tokenClaims;
 
     public RequestDTO(APIGatewayV2CustomAuthorizerEvent input, String secretKey) {
-        if (input.getHeaders() == null) {
-            throw new IllegalArgumentException("Headers are missing");
-        }
-
-        String authHeader = null;
-        for (Map.Entry<String, String> header : input.getHeaders().entrySet()) {
-            if ("authorization".equalsIgnoreCase(header.getKey())) {
-                authHeader = header.getValue();
-                break;
-            }
-        }
-
-        if (authHeader == null) {
+        if (input.getHeaders() == null || !input.getHeaders().containsKey("Authorization")) {
             throw new IllegalArgumentException("Authorization header is missing");
         }
-
         this.httpMethod = input.getRequestContext().getHttp().getMethod();
-        this.resource = input.getRouteKey().replace(httpMethod + " ", "");
-        this.tokenClaims = new TokenClaims(authHeader.replace("Bearer ", ""), secretKey);
+        this.resource = input.getRawPath().replace(httpMethod + " ", "");
+        this.tokenClaims = new TokenClaims(input.getHeaders().get("Authorization").replace("Bearer ", ""), secretKey);
     }
 
     public String getResource() {
--- aws-lambda/CustomAuthorizer/src/main/java/br/com/tp/lncr/aws/lambda/utils/SecretUtils.java
+++ aws-lambda/CustomAuthorizer/src/main/java/br/com/tp/lncr/aws/lambda/utils/SecretUtils.java
@@ -1,27 +1,22 @@
 package br.com.tp.lncr.aws.lambda.utils;
 
-import br.com.tp.lncr.aws.lambda.CustomAuthorizer;
 import br.com.tp.lncr.core.commons.exceptions.OauthException;
 import br.com.tp.lncr.core.commons.utils.Logger;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 
 public class SecretUtils {
 
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(SecretUtils.class);
     private static final String LNCR_AWS_SECRET_KEY = System.getProperty("LNCR_AWS_SECRET_KEY","LNCR_OAUTH_SECRET_KEY");
     private static final String LNCR_AWS_SECRET_NAME = System.getProperty("LNCR_AWS_SECRET_NAME","lncr-prd-sm");
     private static final String LNCR_AWS_REGION = "us-east-1";
 
 
-
     public static String getAwsSecretValue(){
         String localSecretKey = System.getenv(LNCR_AWS_SECRET_KEY);
-        logger.info("localSecretKey: {}", localSecretKey);
         try {
             if (localSecretKey == null || localSecretKey.isEmpty()) {
                 Logger.info("Fetching secret key from AWS Secrets Manager");
@@ -42,7 +37,6 @@ public class SecretUtils {
 
     private static String mapSecretValue(String secretString) {
         try {
-            //logger.info("secretString: {}", secretString);
             JsonNode node = new ObjectMapper().readTree(secretString);
             return node.get(LNCR_AWS_SECRET_KEY).asText();
         } catch (Exception e) {
