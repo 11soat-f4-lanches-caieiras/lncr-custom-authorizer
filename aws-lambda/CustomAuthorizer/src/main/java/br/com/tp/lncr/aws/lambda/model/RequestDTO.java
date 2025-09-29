618cedc552be16bbe574ac2ea49a86067fd0354f
titoparizotto <titoparizotto@gmail.com>
2025-09-29 03:02:38 -0300

    fix: lncr-prd-custom-authorizer - mapeando authorization

--- aws-lambda/CustomAuthorizer/src/main/java/br/com/tp/lncr/aws/lambda/model/RequestDTO.java
+++ aws-lambda/CustomAuthorizer/src/main/java/br/com/tp/lncr/aws/lambda/model/RequestDTO.java
@@ -2,18 +2,33 @@ package br.com.tp.lncr.aws.lambda.model;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2CustomAuthorizerEvent;
 
+import java.util.Map;
+
 public class RequestDTO {
     private final String resource;
     private final String httpMethod;
     private final TokenClaims tokenClaims;
 
     public RequestDTO(APIGatewayV2CustomAuthorizerEvent input, String secretKey) {
-        if (input.getHeaders() == null || !input.getHeaders().containsKey("authorization")) {
-            throw new IllegalArgumentException("authorization header is missing");
+        if (input.getHeaders() == null) {
+            throw new IllegalArgumentException("Headers are missing");
         }
+
+        String authHeader = null;
+        for (Map.Entry<String, String> header : input.getHeaders().entrySet()) {
+            if ("authorization".equalsIgnoreCase(header.getKey())) {
+                authHeader = header.getValue();
+                break;
+            }
+        }
+
+        if (authHeader == null) {
+            throw new IllegalArgumentException("Authorization header is missing");
+        }
+
         this.httpMethod = input.getRequestContext().getHttp().getMethod();
         this.resource = input.getRawPath().replace(httpMethod + " ", "");
-        this.tokenClaims = new TokenClaims(input.getHeaders().get("authorization").replace("Bearer ", ""), secretKey);
+        this.tokenClaims = new TokenClaims(authHeader.replace("Bearer ", ""), secretKey);
     }
 
     public String getResource() {
